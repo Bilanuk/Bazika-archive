c99d1ea9acbc12cf2be4f27cc32ad45333e1d2df
Bilanuk <bilanuk.bill@gmail.com>
2021-12-27 01:04:52 +0200

    added icons for favourites and styles refactor

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,6 @@
-class SearchController < ProfileController
+class SearchController < ApplicationController
     def show
+        @is_favourite = is_favourite? if current_user
         @response = AnilistApiService.show(params[:id])
     end
 
@@ -8,5 +9,11 @@ class SearchController < ProfileController
 
         query = params.dig(:search, :query)
         @response = Kaminari.paginate_array(AnilistApiService.anime(query)).page(params[:page]).per(9)
+    end
+
+    private
+
+    def is_favourite?
+        current_user.titles.exists?(api_id: params[:id])
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :titles, foreign_key: :owner_id, dependent: :destroy
-  # has_many :boards, foreign_key: :owner_id, dependent: :destroy
 end
