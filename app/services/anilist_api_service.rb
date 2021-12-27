3532416fadb8dcfe8527204933fce9ac0b4323a9
Bilanuk <bilanuk.bill@gmail.com>
2021-12-28 00:44:11 +0200

    main page for production

--- app/services/anilist_api_service.rb
+++ app/services/anilist_api_service.rb
@@ -12,8 +12,8 @@ class AnilistApiService
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
       request = Net::HTTP::Post.new(@@url)
       request['content-type'] = 'application/json'
-      request['x-rapidapi-host'] = Rails.application.credentials.AniList_host
-      request['x-rapidapi-key'] = Rails.application.credentials.AniList_key
+      request['x-rapidapi-host'] = 'anilist-graphql.p.rapidapi.com'
+      request['x-rapidapi-key'] = '095e732524mshd74be2bf3d5f145p1501cfjsn7477df35af0a'
       request.body = "{\"query\":\"query {\
         Page (page: 1, perPage: 150) {\
           media (search: \\\"#{search}\\\", isAdult: false, sort: POPULARITY_DESC) {\
@@ -54,8 +54,8 @@ class AnilistApiService
       request = Net::HTTP::Post.new(@@url)
       request = Net::HTTP::Post.new(@@url)
       request['content-type'] = 'application/json'
-      request['x-rapidapi-host'] = Rails.application.credentials.AniList_host
-      request['x-rapidapi-key'] = Rails.application.credentials.AniList_key
+      request['x-rapidapi-host'] = 'anilist-graphql.p.rapidapi.com'
+      request['x-rapidapi-key'] = '095e732524mshd74be2bf3d5f145p1501cfjsn7477df35af0a'
       request.body = "{\"query\":\"query {\
         Media (id: #{id}, isAdult: false) {\
           id\
@@ -111,8 +111,8 @@ class AnilistApiService
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
       request = Net::HTTP::Post.new(@@url)
       request['content-type'] = 'application/json'
-      request['x-rapidapi-host'] = Rails.application.credentials.AniList_host
-      request['x-rapidapi-key'] = Rails.application.credentials.AniList_key
+      request['x-rapidapi-host'] = 'anilist-graphql.p.rapidapi.com'
+      request['x-rapidapi-key'] = '095e732524mshd74be2bf3d5f145p1501cfjsn7477df35af0a'
       request.body = "{\"query\":\"query {\
         Page (page: 1, perPage: 100) {\
           media (sort: POPULARITY_DESC, isAdult: false) {\
